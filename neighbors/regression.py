809572f9f75c06be3b32d382e5caa9ee207497bb
my-lg-gram <w.y.ff@163.com>
2018-10-02 18:08:25 +0800

    add knn regression and unit test

---
+++ neighbors/regression.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from pyml.metrics.pairwise import euclidean_distance
+
+class KNeighborsRegressor():
+    """
+    寻找 向量[x1, x2, x3, ... , xn] -> 向量[y1, y2, y3, ... ym]的映射关系
+
+    Parameters
+    ----------
+    k : int, optional (default = 5)
+        Number of neighbors to use by default.
+
+    distance_func : callable, optional (default = euclidean_distance)
+        distance function used in find the neighbors
+
+        - [callable] : a function like f(vec1,vec2) 
+            which can return the distance(float).
+
+    Notes
+    ------
+    None
+    """
+    def __init__(self, k=5, distance_func=euclidean_distance):
+        self.k = k
+        self.distance_func = distance_func
+
+    def fit(self, X, Y):
+        """
+
+        Parameters
+        -----------
+        X : array-like shape(n_samples, n_features)
+
+        Y : array-like shape(n_samples,, y_features)
+
+        """
+        self.X = X
+        self.Y = Y
+        self.n_samples = self.X.shape[0] # the number of samples
+        self.n_features = self.X.shape[1] # the number of features in trained data
+        self.y_features = self.Y.shape[1] # the number of features in label
+
+    def _predict_single(self, x):
+        """
+        input :
+            x : an numpy array
+        return :
+            y_hat : the prediction of y
+        """
+        distances = np.zeros((self.n_samples))
+        # calculate the distances between x and every sample in self.X
+        for i,stored_x in enumerate(self.X):
+            distances[i] = self.distance_func(stored_x, x)
+        # print(distances)
+
+        # find the indices of the k-smallest values through sort
+        k_min_indexs = np.argsort(distances)[:self.k]
+        k_min_distances = distances[k_min_indexs]
+        # print(k_min_indexs)
+        # 由距离最小的k个x对应的y
+        k_min_ys = self.Y[k_min_indexs]
+        # print("k_min_ys\n", k_min_ys)
+        k_min_distances = distances[k_min_indexs]
+        k_min_distances = np.array(k_min_distances).reshape(-1,1)
+        # 计算概率并归一化
+        final_result = (k_min_ys/(k_min_distances+1)).sum(axis=0)
+        # print(k_min_ys/(k_min_distances+1))
+        # print("k_min_distances\n",k_min_distances)
+        # print(k_min_ys)
+        # print(final_result)
+        final_result = final_result/final_result.sum()
+        return final_result.reshape(1,-1)
+    def predict(self, X_pred):
+        """
+
+        Parameters
+        -------------
+            X_pred : 2d array-like shape(n_samples, n_feature)
+
+        Returns
+        --------------
+            pre_Y : 1d array-like shape(n_samples,)
+
+        """
+        assert(X_pred.shape[1] == self.n_features) # the number of features should be consistent
+        Y_pred = np.zeros((X_pred.shape[0], self.y_features))
+        for i,x_pred in enumerate(X_pred):
+            Y_pred[i] = self._predict_single(x_pred)
+        return Y_pred
---
+++ neighbors/tests/test_regression.py
@@ -0,0 +1,43 @@
+import unittest
+import numpy as np
+
+from pyml.neighbors.regression import KNeighborsRegressor
+
+class test_classification(unittest.TestCase):
+
+    def test_KNeighborsClassifier_no_parameters(self):
+        mini_train_X = np.array([
+            [1,2,3,4,5,6,7,8],
+            [2,3,4,5,6,7,8,9],
+            [3,4,5,6,7,8,9,10],
+            [4,5,6,7,8,9,10,11],
+            [5,6,7,8,9,10,11,12],
+            [6,7,8,9,10,11,12,13],
+            [7,8,9,10,11,12,13,14]
+        ])
+        mini_train_Y = np.array([
+            [1,2,3],
+            [2,3,4],
+            [3,4,5],
+            [4,5,6],
+            [5,6,7],
+            [6,7,8],
+            [7,8,9]
+        ])
+        mini_test_X = np.array([
+            [2,3,4,5,6,7.5,8,9],
+            [4,5,6,7.5,8,9,10,11]
+        ])
+        mini_standard_out_Y = np.array([
+            [0.231982, 0.333333, 0.434685],
+            [0.270308, 0.333333, 0.396359]
+        ])
+        k_clf = KNeighborsRegressor(k=2)
+        k_clf.fit(mini_train_X, mini_train_Y)
+        Y_pred = k_clf.predict(mini_test_X)
+        # 默认，要7位有效数字都要相同,改成了6位
+        np.testing.assert_almost_equal(mini_standard_out_Y, Y_pred, decimal=6)
+
+if __name__ == '__main__':
+    unittest.main()
+
