62385f4b9157f336ce073cdc26ab36e1846d964f
my-lg-gram <w.y.ff@163.com>
2018-10-21 17:53:03 +0800

    add mini-batch in gbdt

--- ensemble/regression.py
+++ ensemble/regression.py
@@ -4,9 +4,45 @@ from pyml.metrics.pairwise import absolute_distance
 from pyml.metrics.regression import pearson_correlation
 import numpy as np
 from pyml.logger import logger
+import math
 
 # TODO: 使用平方误差，还是绝对值误差，还是Huber Loss
 # TODO: 如何给基回归器传参
+
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = None):
+    """
+    X : shape(n_samples,n_features)
+    Y : shape(n_samples,)
+
+    batch:
+    shape(n_samples, n_features) shape(n_samples)
+    """
+    if seed is not None:
+        np.random.seed(seed)
+    X = X.T
+    m = X.shape[1]
+    mini_batches = []
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[permutation].reshape(-1)
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size : (k+1) * mini_batch_size]
+        mini_batch_Y = shuffled_Y[k * mini_batch_size : (k+1) * mini_batch_size]
+        mini_batch = (mini_batch_X.T, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch_Y = shuffled_Y[num_complete_minibatches * mini_batch_size : m]
+        mini_batch = (mini_batch_X.T, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    return mini_batches
+
+
 
 class GradientBoostingRegression():
     def __init__(self,
@@ -16,7 +52,11 @@ class GradientBoostingRegression():
         n_estimators=100,
         max_tree_node_size=10,
         delta=0.5,
-        random_state=None
+        random_state=None,
+        base_divide_way='default',
+        divide_way='half',
+        mini_batch=64,
+        print_interval=10
     ):
         """
         Parameters
@@ -33,6 +73,12 @@ class GradientBoostingRegression():
         self.learning_rate = learning_rate
         self.max_tree_node_size = max_tree_node_size
         self.delta=delta
+        self.base_divide_way = base_divide_way
+        self.divide_way = divide_way
+        self.mini_batches = []
+        self.current_mini_batch_index = 0
+        self.mini_batch = mini_batch
+        self.print_interval = print_interval
         # key='f' : a list of estimator
         # key='lr' : a list of learning_rate
         self.information = {
@@ -43,6 +89,20 @@ class GradientBoostingRegression():
             'f' : [],
             'lr' : []
         }
+
+    def init_mini_batches(self, X, Y):
+        """
+        X : 2d array-like shape(n_samples,n_features)
+        Y : 2d array-like shape(n_samples,1)
+        """
+        self.mini_batches = random_mini_batches(X, Y, self.mini_batch)
+        self.current_mini_batch_index = 0
+
+    def get_mini_batch(self):
+        current_mini_batch = self.mini_batches[self.current_mini_batch_index]
+        return current_mini_batch
+    def update_mini_batch(self):
+        self.current_mini_batch_index = (self.current_mini_batch_index+1) % len(self.mini_batches)
 
     def optimizer(self, X, Y, watch=False):
         """
@@ -92,7 +152,10 @@ class GradientBoostingRegression():
         self.information['gradient'].append(d_fx)
 
         # 创建一个新回归器，去拟合梯度
-        new_estimator = self.base_estimator(max_node_size=self.max_tree_node_size)
+        new_estimator = self.base_estimator(
+            max_node_size=self.max_tree_node_size,
+            divide_way=self.divide_way
+        )
         new_estimator.fit(X,d_fx)
         self.parameters['f'].append(new_estimator)
         self.parameters['lr'].append(lr)
@@ -100,37 +163,44 @@ class GradientBoostingRegression():
 
     def fit(self, X, Y,watch=False):
         logger.debug('X : \n{} Y : {}'.format(X, Y))
-        init_estimator = self.base_estimator(max_node_size=self.max_tree_node_size)
+        init_estimator = self.base_estimator(
+            max_node_size=self.max_tree_node_size,
+            divide_way=self.base_divide_way
+            )
         init_estimator.fit(X,Y)
         self.parameters['f'].append(init_estimator)
         self.parameters['lr'].append(1)
         for i in range(self.n_estimators):
             cost = self.optimizer(X,Y)
-            if i % 1 == 0:
+            if i % self.print_interval == 0:
                 logger.info('train {}/{}  current cost: {}'.format(i,self.n_estimators,cost))
                 # print('train {}/{}  current cost : {}'.format(i,self.n_estimators,cost))
 
-    def fit_and_valid(self, X, Y,X_valid,Y_valid, mini_batch=0, watch=False):
+    def fit_and_valid(self, X, Y,X_valid,Y_valid, watch=False):
+        self.init_mini_batches(X,Y)
         logger.debug('X : \n{} Y : {}'.format(X, Y))
-        init_estimator = self.base_estimator(max_node_size=self.max_tree_node_size)
+        init_estimator = self.base_estimator(
+            max_node_size=self.max_tree_node_size,
+            divide_way=self.base_divide_way
+        )
         init_estimator.fit(X,Y)
         self.parameters['f'].append(init_estimator)
         self.parameters['lr'].append(1)
         for i in range(self.n_estimators):
-            if mini_batch==0:
+            if self.mini_batch==0:
                 # 使用全部样例：
                 cost = self.optimizer(X,Y)
             else :
                 # 使用mini_batch个样例：
-                all_indices = np.arange(0, X.shape[0])
-                np.random.shuffle(all_indices)
-                batch_indices = all_indices[:mini_batch]
-                cost = self.optimizer(X[batch_indices], Y[batch_indices])
-            if i % 1 == 0:
+                X_batch,Y_batch = self.get_mini_batch()
+                print('!',X_batch.shape,Y_batch.shape)
+                cost = self.optimizer(X_batch, Y_batch)
+            if i % self.print_interval == 0:
                 this_loss =  self.get_test_cost(X_valid, Y_valid)
                 self.information['test_loss'].append(this_loss)
                 logger.info('train {}/{}  current cost: {}, test: {}'.format(i,self.n_estimators,cost,this_loss))
                 # print('train {}/{}  current cost : {}'.format(i,self.n_estimators,cost))
+            self.update_mini_batch()
 
     def get_test_cost(self, X, Y):
         Y_pred = self.predict(X)
